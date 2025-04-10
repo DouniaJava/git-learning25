8a64cd4ade87698d9c5b16fff8c8f7e60ebdd481
Dounia <dounilion@hotmail.com>
2025-04-10 15:59:12 -0400

    jonny-added divid method ,updated project to maven .

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -1,3 +1,4 @@
+
 package com.git.learning;
 public class Calculator {
 public int  add(int x,int y) {
@@ -10,6 +11,16 @@ public int multiply(int x,int y) {
 	return x*y;
 
 }
+public int divid(intx,int y) {
+	
+	int result=0
+	if(y!=0) {
+	result=	x/y;
+	}else {
+		throw new Exception (" you cannot divide number by Zero.");
+	}
+}
+
 
 }
 
